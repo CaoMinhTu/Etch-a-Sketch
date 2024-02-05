48d00a9a1cdd59a7f5a6aafd25a249ef529684fa
Cao Minh Tu <caominhtuvn@gmail.com>
2024-02-05 23:41:05 +0700

    Create function to set border width for grid
    When user set arbitrary cells per side, grid width will not be divisible
    for cell number, resulting in leftover. This leftover will be
    distributed to the border widths.

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-const GRID_DIMENSION = "512px"; // grid width and height
+const GRID_DIMENSION = 512; // grid width and height in pixels
+const GRID_COLOR = "#505050";
 
 // đặt kích thước grid
-document.getElementById('grid').style.height = GRID_DIMENSION;
-document.getElementById('grid').style.width = GRID_DIMENSION;
+let grid = document.getElementById('grid');
+grid.style.height = GRID_DIMENSION + "px";
+grid.style.width = GRID_DIMENSION + "px";
+grid.style.borderColor = GRID_COLOR;
+grid.style.borderStyle = "solid";
 
-let cellsPerSide = 16;
+function setGridBordersWidth(gridDimension, cellsPerSide) {
+    // tính độ rộng còn dư phải phân bổ cho hai biên
+    let leftOverWidth = gridDimension % cellsPerSide;
+
+    let grid = document.getElementById('grid');
+
+    // đặt độ rộng biên trái và trên
+    let leftBorderWidth = Math.floor(leftOverWidth / 2);
+    grid.style.borderLeftWidth = leftBorderWidth + 'px';
+    grid.style.borderTopWidth = leftBorderWidth + 'px';
+
+    // đặt độ rộng biên phải và dưới
+    let rightBorderWidth = leftOverWidth - leftBorderWidth;
+    grid.style.borderRightWidth = rightBorderWidth + 'px';
+    grid.style.borderBottomWidth = rightBorderWidth + 'px';    
+}
 
 function createCells(cellsPerSide) {
 
 }
+
+let cellsPerSide = 18;
+
+setGridBordersWidth(GRID_DIMENSION, cellsPerSide);
+
