120f705e320d1519e063587a7fddacd4dc2f42a3
Cao Minh Tu <caominhtuvn@gmail.com>
2024-02-05 22:40:37 +0700

    Change the main unit into cells per side

---
+++ script.js
@@ -0,0 +1,11 @@
+const GRID_DIMENSION = "512px"; // grid width and height
+
+// đặt kích thước grid
+document.getElementById('grid').style.height = GRID_DIMENSION;
+document.getElementById('grid').style.width = GRID_DIMENSION;
+
+let cellsPerSide = 16;
+
+function createCells(cellsPerSide) {
+
+}
