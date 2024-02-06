6259f7da57b75263eaa7d9203d7c91c22c559e0c
Cao Minh Tu <caominhtuvn@gmail.com>
2024-02-06 21:50:27 +0700

    Add cells to grid

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 const GRID_DIMENSION = 512; // grid width and height in pixels
 const GRID_COLOR = "#505050";
+const CELL_BORDER_WIDTH = 1; // cell border width in pixels
 
-// đặt kích thước grid
-let grid = document.getElementById('grid');
+// set grid properties
+let grid = document.querySelector('#grid');
 grid.style.height = GRID_DIMENSION + "px";
 grid.style.width = GRID_DIMENSION + "px";
 grid.style.borderColor = GRID_COLOR;
 grid.style.borderStyle = "solid";
 
 function setGridBordersWidth(gridDimension, cellsPerSide) {
-    // tính độ rộng còn dư phải phân bổ cho hai biên
+    // calculate left over width that will be distributed to borders
     let leftOverWidth = gridDimension % cellsPerSide;
+    console.log('leftOverWidth: ', leftOverWidth);
 
-    let grid = document.getElementById('grid');
+    let grid = document.querySelector('#grid');
 
-    // đặt độ rộng biên trái và trên
+    // set width of border left and top
     let leftBorderWidth = Math.floor(leftOverWidth / 2);
     grid.style.borderLeftWidth = leftBorderWidth + 'px';
     grid.style.borderTopWidth = leftBorderWidth + 'px';
 
-    // đặt độ rộng biên phải và dưới
+    // set width of border right and bottom
     let rightBorderWidth = leftOverWidth - leftBorderWidth;
     grid.style.borderRightWidth = rightBorderWidth + 'px';
     grid.style.borderBottomWidth = rightBorderWidth + 'px';    
 }
 
 function createCells(cellsPerSide) {
+    const cellSize = Math.floor(GRID_DIMENSION / cellsPerSide); // cell size in pixels
+    console.log('cellSize: ', cellSize);
 
+    let grid = document.querySelector('#grid');
+    for (let i = 0; i < cellsPerSide * cellsPerSide; i++) {
+        let cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.style.width = cellSize + 'px';
+        cell.style.height = cellSize + 'px';
+        cell.style.borderWidth = CELL_BORDER_WIDTH + 'px';
+        cell.style.borderColor = GRID_COLOR;
+        cell.style.backgroundColor = 'white';
+        grid.appendChild(cell);
+    }
 }
 
-let cellsPerSide = 18;
+let cellsPerSide = 10;
 
 setGridBordersWidth(GRID_DIMENSION, cellsPerSide);
 
+createCells(cellsPerSide);
